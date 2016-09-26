1cf587c213dd042c6fd3e42b0f9439bb0d0a7bdb
peterfei <peterfeispace@gmail.com>
2016-09-26 11:07:57 +0800

    Initial Commit

---
+++ NewsTopic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsTopic
+//
+//  Created by peterfei on 2016/9/26.
+//  Copyright © 2016年 maplocation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
